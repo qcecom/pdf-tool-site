4788dace2134214a3c12602691d9b2cad6f490b7
qcecom <quoccuongecom@gmail.com>
2025-08-15 00:11:08 +0700

    chore: convert next config to typescript

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'standalone',
   trailingSlash: false,
   webpack: (config) => {
@@ -7,4 +8,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
